0c4ef48ec328ce13f8831268b4819817079105de
opportus <opportus@gmail.com>
2017-10-27 21:23:37 +0200

    Improves EntityManager

--- src/EntityManager.php
+++ src/EntityManager.php
@@ -2,8 +2,6 @@
 
 namespace Opportus\Orm;
 
-use \Exception;
-
 /**
  * The entity manager...
  *
@@ -14,14 +12,14 @@ use \Exception;
 class EntityManager
 {
 	/**
-	 * @var array $entities
+	 * @var array $services
 	 */
-	protected $entities = array();
+	protected $services = array();
 
 	/**
 	 * Registers an entity.
 	 *
-	 * @param array $entity Structured as such:
+	 * @param array $entityDefinition Structured as such:
 	 *    array(
 	 *         'name'       => '',
 	 *         'properties' => array(
@@ -35,39 +33,47 @@ class EntityManager
 	 *
 	 * @param GatewayInterface $gateway
 	 */
-	public function register(array $entity, GatewayInterface $gateway)
+	public function registerEntity(array $entityDefinition, GatewayInterface $gateway)
 	{
-		$entityName          = $entity['name'];
-		$properties          = array_fill_keys(array_keys($entity['properties']), null);
+		$properties          = array_fill_keys(array_keys($entityDefinition['properties']), null);
 		$validationCallbacks = array();
 		$maps                = array();
 
-		foreach ($entity['properties'] as $property => $definition) {
-			$validationCallbacks[$property] = $definition['validationCallback'];
-			$maps[$property]['table']       = $definition['table'];
-			$maps[$property]['column']      = $definition['column'];
+		foreach ($entityDefinition['properties'] as $property => $propertyDefinition) {
+			$validationCallbacks[$property] = $propertyDefinition['validationCallback'];
+			$maps[$property]['table']       = $propertyDefinition['table'];
+			$maps[$property]['column']      = $propertyDefinition['column'];
 		}
 
 		$mapper     = new Mapper($gateway, $maps);
 		$factory    = new Factory($properties, $validationCallbacks);
 		$repository = new Repository($mapper, $factory);
 
-		$this->entities[$entityName] = new Entity($entityName, $mapper, $factory, $repository);
+		$this->services[$entityDefinition['name']] = array(
+			'mapper'     => $mapper,
+			'factory'    => $factory,
+			'repository' => $repository
+		);
 	}
 
 	/**
-	 * Gets an entity.
+	 * Getter overload.
 	 *
-	 * @param  string $entity
-	 * @return Entity
-	 * @throws Exception If entity is not registered
+	 * @param  string $name
+	 * @param  string $arguments
+	 * @return mixed
 	 */
-	public function get($entity)
+	public function __call($name, $arguments)
 	{
-		if (! isset($this->entities[$entity])) {
-			throw new Exception('Entity not registered');
-		}
+		$entities = array_map('ucfirst', array_keys($this->services));
 
-		return $this->entities[$entity];
+		if (preg_match('/^get(' . implode('|', $entities) . ')(.+)$/', $name, $matches)) {
+			$matches = array_map('strtolower', $matches);
+
+			if (array_key_exists($matches[2], $this->services[$matches[1]])) {
+				return $this->services[$matches[1]][$matches[2]];
+			}
+		}
 	}
 }
+
